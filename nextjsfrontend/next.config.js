4fecb13f654e3d5840e42abbf0b5b41dfe665f06
Ameen Salim <ameensalim1@gmail.com>
2024-12-09 13:19:11 -0800

    ameen - changed model to 4omini

--- nextjsfrontend/next.config.js
+++ nextjsfrontend/next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  env: {
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080",
+  },
 };
 
 module.exports = nextConfig;
